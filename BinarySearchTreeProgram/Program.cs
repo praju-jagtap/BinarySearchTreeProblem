83f5e3ee018947538af49084827192971166bdb3
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-17 10:46:50 +0530

    [Prajakta] Add: Display Welcome Message

---
+++ BinarySearchTreeProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTreeProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Binary Search Tree Problem Statement");
+        }
+    }
+}
